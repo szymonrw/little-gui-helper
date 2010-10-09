31333ca4ba84b88ed1e90cac96665317204f5a16
Szymon Witamborski <santamon@gmail.com>
2010-10-09 15:21:13 +0200

    Changed to stable version of Clojure, Clojure-contrib and Swank (1.2.0)

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject little-gui-helper "0.1.0-SNAPSHOT"
   :description "Helper functions and macros for building GUIs with SWT and Swing in Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0-alpha1"]
-		 [org.clojure.contrib/string "1.3.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]
 		     [org.eclipse/swt-gtk-linux-x86 "3.5.2"]])
